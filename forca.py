137483fa01281783b285acdcf98084f3ff948998
Eduardo <edusartori00@gmail.com>
2023-01-12 17:36:37 -0300

    Ajustando listas

--- forca.py
+++ forca.py
@@ -3,26 +3,39 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "banana".upper()
+    letras_corretas = ["_" for letra in palavra_secreta]
 
-    enforcou = False
-    acertou = False
+    enforcou, acertou = False
+    erros = 0
 
-    print(letras_acertadas)
+    print(f"A palavra possui: {letras_corretas}")
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_corretas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print(f"Ops, você errou!\n Você ainda possui {6 - erros} tentaticas.")
 
-        print(letras_acertadas)
+        enforcou = erros == 6
+        acertou = "_" not in letras_corretas
+        print(letras_corretas)
+
+    if (acertou):
+        print("Parabéns, você acertou a palavra secreta!")
+    else:
+        print("Que pena, suas chances terminaram e você não conseguiu acertar a palavra secreta")
+        print(f"A palavra secreta era {palavra_secreta}")
+    print("-------------------- \n Fim do jogo!")
 
     print("Fim do jogo")
 
